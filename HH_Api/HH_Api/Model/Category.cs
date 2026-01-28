4dcd17e9a39befc6ba8e9e6f24c413bfb9ff0db5
Dodo-hbsz <simon.dominik@hbsz.edu.hu>
2026-01-28 12:44:02 +0100

    Add files via upload

---
+++ HH_Api/HH_Api/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HH_Api.Model
+{
+    [Table("category")]
+    public class Category
+    {
+        [Key]
+        [Column("cname")]
+        public required string Name { get; set; }
+    }
+}
---
+++ HH_Api/HH_Api/Model/Subcategory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Org.BouncyCastle.Crypto.Engines.SM2Engine;
+
+namespace HH_Api.Model
+{
+    [Table("subcategory")]
+    public class Subcategory
+    {
+        [Key]
+        [Column("scname")]
+        public required string Name { get; set; }
+
+        [Column("cname")]
+        public string? CName { get; set; }
+
+
+        [ForeignKey(nameof(CName))]
+        public Category? Category { get; set; }
+    }
+}
